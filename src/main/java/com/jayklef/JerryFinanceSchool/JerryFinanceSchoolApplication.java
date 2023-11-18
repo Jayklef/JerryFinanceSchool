4682c302774a9fe7182e9d52548c0a5b7241a257
Jerry Arhawho <jerryarhawho@gmail.com>
2023-11-18 13:39:41 +0000

    Initial Project Setup

---
+++ src/main/java/com/jayklef/JerryFinanceSchool/JerryFinanceSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.JerryFinanceSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerryFinanceSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerryFinanceSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayklef/JerryFinanceSchool/JerryFinanceSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.JerryFinanceSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerryFinanceSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
