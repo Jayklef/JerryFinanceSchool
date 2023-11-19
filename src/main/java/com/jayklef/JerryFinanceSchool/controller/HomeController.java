07acc5e79550756d769d402689c506a133692e50
Jerry Arhawho <jerryarhawho@gmail.com>
2023-11-19 23:58:48 +0000

    UI template

--- src/main/java/com/jayklef/JerryFinanceSchool/controller/HomeController.java
+++ src/main/java/com/jayklef/JerryFinanceSchool/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.jayklef.JerryFinanceSchool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +9,7 @@ public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
     public String displayHomePage(){
+        //model.addAttribute("username","Jerry");
         return "home.html";
     }
 }
