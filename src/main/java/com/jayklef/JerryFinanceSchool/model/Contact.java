b194e96f3c16775c7b2a6cf78b4e8c87912b1c46
Jerry Arhawho <jerryarhawho@gmail.com>
2023-11-21 08:25:04 +0000

    Validations

--- src/main/java/com/jayklef/JerryFinanceSchool/model/Contact.java
+++ src/main/java/com/jayklef/JerryFinanceSchool/model/Contact.java
@@ -1,14 +1,30 @@
 package com.jayklef.JerryFinanceSchool.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be 3 or more character")
     public String name;
+
+    @NotBlank
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Phone number must be 11 digits")
     public String phoneNumber;
+
+    @Email(message = "Please enter a valid email")
     private String email;
+
+    @NotBlank(message = "Subject cannot be blank")
     public String subject;
+
+    @NotBlank(message = "Message cannot be blank")
+    @Size(min = 5, message = "Message mast have above 5 characters")
     public String message;
 
 }
