391d775c72c8129f13740ca7a8f2c3d61e5dae87
Jerry Arhawho <jerryarhawho@gmail.com>
2023-11-23 23:38:53 +0000

    dashboard and exception

---
+++ src/main/java/com/jayklef/JerryFinanceSchool/controller/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.jayklef.JerryFinanceSchool.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(Exception exception){
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        errorPage.addObject("errorMessage", exception.getMessage());
+
+        return errorPage;
+    }
+}
